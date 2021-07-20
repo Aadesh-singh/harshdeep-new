0cc01d3da9cf45bb8af7c04c81ccbf8269cf0829
infuzeX <68704045+infuzeX@users.noreply.github.com>
2021-07-20 15:40:55 +0530

    Add files via upload

---
+++ cdn/growthschool/razorpay-custom-webflow.js
@@ -0,0 +1,27 @@
+// Author: Prateek Kumar
+// Email: prateek@epyc.in
+// Website: https://askprateek.com
+
+
+function makeRazorpayPayment(index) {
+  var razorpayButtons = $(".razorpay-payment-button .PaymentButton");
+  razorpayButtons[index].click();
+}
+
+function attachPaymentListener() {
+  const button1 = $("[data-payment='1']");
+  const button2 = $("[data-payment='2']");
+
+  const buttons = [button1, button2];
+  buttons.forEach(function (button, index) {
+    if (button) {
+          button.on("click", function () {
+            makeRazorpayPayment(index);
+          });
+    }
+
+  });
+}
+$(document).ready(function () {
+  attachPaymentListener();
+});
